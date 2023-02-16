c6c0369386f606d03282caf08fbb3eb5486d836a
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-16 15:19:41 -0500

    Thursday

---
+++ breedFetcher.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const args = process.argv.slice(2);
+
+const searchCats = (args) => {
+  const [breed] = args;
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      let data = JSON.parse(body);
+      if (data.length === 0) {
+        console.log('no data found');
+      } else {
+        console.log('body:', data); // Print the HTML for the Google homepage
+      }
+    }
+  });
+};
+searchCats(args);
+
+module.exports = searchCats;
